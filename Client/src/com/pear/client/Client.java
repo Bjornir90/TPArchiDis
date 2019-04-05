84330b9ab1bb1274513a58e361581acb19b1b7b7
Elian <elianraynaud@gmail.com>
2019-04-05 16:28:20 +0200

    Finish Client

--- Client/src/com/pear/client/Client.java
+++ Client/src/com/pear/client/Client.java
@@ -8,13 +8,15 @@ import java.rmi.registry.Registry;
 
 public class Client {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         /* AH OUI OUI AH OUI OUI */
 
-        Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+        Registry registry = LocateRegistry.getRegistry("172.18.86.148", 1099);
         Catalog catalog = (Catalog) registry.lookup("catalog");
 
-        
+        System.out.println("Le nombre d'articles dans le catalogue est : " + catalog.getNumberArticles());
+
+
 
     }
 
